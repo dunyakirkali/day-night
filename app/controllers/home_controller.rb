4aaeb42f244b196b57c508b4d2f5592771381707
Dunya Kirkali <dunyakirkali@gmail.com>
2013-03-19 17:30:08 +0100

    Added sunrise sunset gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,8 @@ class HomeController < ApplicationController
     @country = request.location.country_code
     @latitude = request.location.latitude
     @longitude = request.location.longitude
+
+    sss = SunRiseSet.now(@latitude, @longitude).to_s
   end
   def poll
   end
