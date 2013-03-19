cf4dc2d4157f72971a8d63dcaa4be79846302978
Dunya Kirkali <dunyakirkali@gmail.com>
2013-03-19 13:43:30 +0100

    Initial Rails 3.2.12 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DayAndNight::Application.config.secret_token = 'dce465ff99780fdf66740ffb80beefc437a871dd984d725cd80e3e2153f427494003bfc985e3bde625e7d7b43e0f253d5298f7e97b73ae34cceffc77378107ae'
