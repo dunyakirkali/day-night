1a61dcad85f452fc4593ecd59604f85697254c2e
Dunya Kirkali <dunyakirkali@gmail.com>
2013-03-19 16:49:12 +0100

    Added idea map to gitignore Added geolocation gem Added lat long detection from ip

--- Gemfile
+++ Gemfile
@@ -20,3 +20,4 @@ end
 gem 'jquery-rails'
 gem 'slim'
 gem 'unicorn'
+gem 'geocoder'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   respond_to :js, :only => :poll
   def index
+    @city = request.location.city
+    @country = request.location.country_code
+    @latitude = request.location.latitude
+    @longitude = request.location.longitude
   end
   def poll
   end
